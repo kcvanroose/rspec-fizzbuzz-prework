a577d3f8d445f3db14c799c6f252f055bc9eab15
casey van-roose <kcvanroose@gmail.com>
2018-06-28 13:30:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(number)
-  if 
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
   elsif number % 5 == 0 
     return "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
-    number % 3 == 0 
+  elsif  number % 3 == 0 
     return "Fizz"
   end
 end
